6bb4c2efbe3e1f28135c982c500886d4b78ebe77
Yousra0225 <chbibyousra02@gmail.com>
2025-07-22 15:45:27 +0200

    feat(app-controller): add app controller class

---
+++ src/Controller/AppController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/{name}")
+     * @return Response
+     */
+    public function index(string $name): Response
+    {
+        dd($name);
+        return $this->render('base.html.twig');
+    }
+}
